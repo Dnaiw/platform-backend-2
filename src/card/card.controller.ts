2c8f4ab2e37492b51f4bad13f3223da48c0e28fd
Denis Kudryavcev <dnk160604@gmail.com>
2023-01-28 22:02:25 +0300

    crud_done

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -6,10 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { CardService } from './card.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { CreateDeckDto } from './dto/create-deck.dto';
+import { UpdateDeckDto } from './dto/update-deck.dto';
 
 @Controller('card')
 export class CardController {
@@ -20,9 +23,19 @@ export class CardController {
     return this.cardService.create(createCardDto);
   }
 
+  @Post('newDeck')
+  createDeck(@Body() createDeckDto: CreateDeckDto) {
+    return this.cardService.createDeck(createDeckDto);
+  }
+
   @Get()
   findAll() {
     return this.cardService.findAll();
+  }
+
+  @Get('/getCardsFromDeck')
+  getCardsFromDeck(@Query('deckId') deckId: string) {
+    return this.cardService.getCardsFromDeck(deckId);
   }
 
   @Get(':id')
@@ -30,9 +43,22 @@ export class CardController {
     return this.cardService.findOne(id);
   }
 
+  @Patch('/updateDeck')
+  updateDeck(
+    @Query('deckId') deckId: string,
+    @Body() updateDeckDto: UpdateDeckDto,
+  ) {
+    return this.cardService.updateDeck(deckId, updateDeckDto);
+  }
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCardDto: UpdateCardDto) {
     return this.cardService.update(id, updateCardDto);
+  }
+
+  @Delete('/deleteDeck')
+  removeDeck(@Query('deckId') deckId: string) {
+    return this.cardService.removeDeck(deckId);
   }
 
   @Delete(':id')
--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { CreateDeckDto } from './dto/create-deck.dto';
+import { UpdateDeckDto } from './dto/update-deck.dto';
 import { firestore } from 'firebase-admin';
+import { identity } from 'rxjs';
 
 @Injectable()
 export class CardService {
   private cardsCollection = firestore().collection('cards');
+  private decksCollection = firestore().collection('decks');
 
   create(createCardDto: CreateCardDto) {
     const card: Omit<CreateCardDto, 'id'> = {
@@ -16,8 +20,22 @@ export class CardService {
     return this.cardsCollection.doc(createCardDto.id).set(card);
   }
 
-  findAll() {
-    return `This action returns all card`;
+  createDeck(createDeckDto: CreateDeckDto) {
+    const deck: Omit<CreateDeckDto, 'id'> = {
+      name: createDeckDto.name,
+      gameId: createDeckDto.gameId,
+    };
+    return this.decksCollection.doc(createDeckDto.id).set(deck);
+  }
+
+  async findAll() {
+    const snapshot = await this.cardsCollection.get();
+    return snapshot.docs.map((doc) => doc.data());
+  }
+
+  async getCardsFromDeck(id: string) {
+    const snapshot = await this.cardsCollection.where('deckId', '==', id).get();
+    return snapshot.docs.map((doc) => doc.data());
   }
 
   async findOne(id: string) {
@@ -27,14 +45,28 @@ export class CardService {
       throw `Card with id ${id} doesn't exist`;
     }
 
-    return doc;
+    return doc.data();
   }
 
   update(id: string, updateCardDto: UpdateCardDto) {
     return this.cardsCollection.doc(id).set(updateCardDto);
   }
 
+  updateDeck(id: string, updateDeckDto: UpdateDeckDto) {
+    return this.decksCollection.doc(id).set(updateDeckDto);
+  }
+
   remove(id: string) {
     return this.cardsCollection.doc(id).delete();
+  }
+
+  async removeDeck(id: string) {
+    const snapshot = await this.cardsCollection.where('deckId', '==', id).get();
+    const documents = snapshot.docs;
+    let i: number;
+    for (i = 0; i < documents.length; i++) {
+      this.cardsCollection.doc(documents[i].id).delete();
+    }
+    return this.decksCollection.doc(id).delete();
   }
 }
